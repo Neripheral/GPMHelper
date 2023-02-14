127a0f806dffe77752b58bd422df8d04a56caed0
Neripheral <neripheral@gmail.com>
2023-02-14 17:42:36 +0100

    feat: relegate document.ready to main method

--- orders.js
+++ orders.js
@@ -1,18 +1,15 @@
+(() => {
+    $(document).ready(function(){
+        main();
+    });
+})();
+
 function onResponseReceived(responseText){
     const pattern = /.*Telefon<\/div>[^>]+>(?<phone>[^<]*)<[^E]*Email[^:]*:(?<email>[^"]*)[^A]*Adres dostawy(?:[^>]*>){2}(?<name>[^<]*)(?:[^>]*>){4}(?<street>[^<]*)(?:[^>]*>){2}(?<postalCode>\d\d-\d\d\d) (?<city>[^<]*)[^P]*Paczkomat(?:[^>]*>){2}\s*(?<boxId>[^<\s]*)\s*<(?:[^>]*>){2}\s*(?<boxAddress>[^<]*\S)\s*</;
     var found = responseText.match(pattern);
     console.log(found);
 }
 
+function main(){
 
-(() => {
-    //OrdersManager.fetchOrderHtml("https://gpm.pl/shop/admin/order-details/id/55914", onResponseReceived);
-    $(document).ready(function(){
-        var inPost = new InPostManager(PrivateData.organizationId, PrivateData.token);
-        inPost.tryConnect(function(response){response == false ? console.log("401: Unauthorized!") : 
-            inPost.getShipments(function(response){console.log(JSON.parse(response));});    
-        });
-        
-        inPost.addShipment(OrdersManager.getExampleOrder(), function(response){console.log(response);});
-    });
-})();
+}
