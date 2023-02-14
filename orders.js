a38e279aa76d3573b5fd37327462835fd402ca62
Neripheral <neripheral@gmail.com>
2023-02-14 22:37:44 +0100

    mod: make the main loop asynchronous

--- orders.js
+++ orders.js
@@ -4,27 +4,34 @@
     });
 })();
 
-function main(){
-    $(".gpmlist tbody tr").each(function(index, e){
-        var id = $(e).attr("id").match(/so_row_(?<id>\d*)/)[1];
-        if(id > 55970)
-            OrdersManager.fetchOrderById(id, function(html){
-                var order = OrdersManager.parseHtmlToOrder(html);
-                if(order == false)
-                    return false;
+function loadFrom(index){
+    console.log(".gpmlist tbody tr:nth-child(" + index + ")");
+    var e = $(".gpmlist tbody tr:nth-child(" + index + ")");
+        if(e == null)
+            return;
+    var id = $(e).attr("id").match(/so_row_(?<id>\d*)/)[1];
+    if(index < 100)
+        OrdersManager.fetchOrderById(id, function(html){
+            var order = OrdersManager.parseHtmlToOrder(html);
+            loadFrom(index+1);
+            if(order == false)
+                return false;
 
-                OrdersManager.addInPostIcon(id, function(){
-                    InPostManager.addShipment(order, function(response){
-                        if(response.readyState == 4 && response.status > 200 && response.status < 300)
-                            OrdersManager.markRowAsSent(id);
-                        else if(response.readyState == 4 && response.status >= 400){
-                            var responseText = JSON.parse(response.responseText);
-                            OrdersManager.markRowAsError(id, responseText.details);
-                            console.log(responseText);
-                        }
-                    });
+            OrdersManager.addInPostIcon(id, function(){
+                InPostManager.addShipment(order, function(response){
+                    if(response.readyState == 4 && response.status > 200 && response.status < 300)
+                        OrdersManager.markRowAsSent(id);
+                    else if(response.readyState == 4 && response.status >= 400){
+                        var responseText = JSON.parse(response.responseText);
+                        OrdersManager.markRowAsError(id, responseText.details);
+                        console.log(responseText);
+                    }
                 });
-                console.log(order);
             });
-    });
+            console.log(order);
+        });
+}
+
+function main(){
+    loadFrom(1);
 }
