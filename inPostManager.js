d3f39f045bcb617e0f3f55a6ce8ada7a274e316d
Neripheral <neripheral@gmail.com>
2023-02-14 17:35:49 +0100

    feat: make addShipment cooperate with InPost db

--- inPostManager.js
+++ inPostManager.js
@@ -1,7 +1,7 @@
 class InPostManager{
     constructor(companyId, token){
         this.companyId = companyId;
-        this.token = token + "fs";
+        this.token = token;
         this.root = "https://sandbox-api-shipx-pl.easypack24.net/v1/";
         //this.root = "https://api-shipx-pl.easypack24.net/v1/";
     }
@@ -15,9 +15,72 @@ class InPostManager{
                 onResponseReceived(false);
             }
         }
-        var url = this.root + "organizations/" + this.organizationId;
+        var url = this.root + "organizations/" + this.companyId;
         xhttp.open("GET", url, "true");
         xhttp.setRequestHeader("Authorization", 'Bearer ' + this.token);
         xhttp.send();
+    }
+
+    getShipments(onResponseReceived){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            onResponseReceived(this.responseText);
+        }
+        var url = this.root + "organizations/" + this.companyId + "/shipments";
+        xhttp.open("GET", url, "true");
+        xhttp.setRequestHeader("Authorization", 'Bearer ' + this.token);
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.send();
+    }
+
+    /**
+     * 
+     * @param {*} order Order object. It consists of:
+     *              lockerId
+     *              email
+     *              phone          must consist of 9 digits
+     *              first_name     
+     *              last_name      
+     *              city      
+     *              building_number
+     *              street
+     *              post_code
+     * @param {*} onResponseReceived 
+     */
+    addShipment(order, onResponseReceived){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            onResponseReceived(this.responseText);
+        }
+        var url = this.root + "organizations/" + this.companyId + "/shipments";
+        xhttp.open("POST", url, "true");
+        xhttp.setRequestHeader("Authorization", 'Bearer ' + PrivateData.token);
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        var text = JSON.stringify({
+            "service": "inpost_locker_standard",
+            "custom_attributes": {
+                "target_point": order.lockerId
+            },
+            "parcels":[
+                {
+                    "template": "small",
+                    "tracking_number": null
+                }
+            ],
+            "receiver": {
+                "first_name": order.first_name,
+                "last_name": order.last_name,
+                "email": order.email,
+                "phone": order.phone,
+                "address": {
+                    "street": order.street,
+                    "building_number": order.building_number,
+                    "city": order.city,
+                    "post_code": order.post_code,
+                    "country_code": "PL"
+                }
+            }
+        });
+        xhttp.send(text);
     }
 }
--- orders.js
+++ orders.js
@@ -1,23 +1,29 @@
-class OrdersManager{
-    static fetchOrderHtml = function(url, onResponseReceived){
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function(){
-            if(this.readyState == 4 && this.status == 200){
-                onResponseReceived(this.responseText);
-            }
-        }
-        xhttp.open("GET", url, "true");
-        xhttp.send();
-    }
-
+function onResponseReceived(responseText){
+    const pattern = /.*Telefon<\/div>[^>]+>(?<phone>[^<]*)<[^E]*Email[^:]*:(?<email>[^"]*)[^A]*Adres dostawy(?:[^>]*>){2}(?<name>[^<]*)(?:[^>]*>){4}(?<street>[^<]*)(?:[^>]*>){2}(?<postalCode>\d\d-\d\d\d) (?<city>[^<]*)[^P]*Paczkomat(?:[^>]*>){2}\s*(?<boxId>[^<\s]*)\s*<(?:[^>]*>){2}\s*(?<boxAddress>[^<]*\S)\s*</;
+    var found = responseText.match(pattern);
+    console.log(found);
 }
 
-(() => {
-    function onResponseReceived(responseText){
-        const pattern = /.*Telefon<\/div>[^>]+>(?<phone>[^<]*)<[^E]*Email[^:]*:(?<email>[^"]*)[^A]*Adres dostawy(?:[^>]*>){2}(?<name>[^<]*)(?:[^>]*>){4}(?<street>[^<]*)(?:[^>]*>){2}(?<postalCode>\d\d-\d\d\d) (?<city>[^<]*)[^P]*Paczkomat(?:[^>]*>){2}\s*(?<boxId>[^<\s]*)\s*<(?:[^>]*>){2}\s*(?<boxAddress>[^<]*\S)\s*</;
-        var found = responseText.match(pattern);
-        console.log(found);
-    }
 
-    OrdersManager.fetchOrderHtml("https://gpm.pl/shop/admin/order-details/id/55914", onResponseReceived);
+(() => {
+    //OrdersManager.fetchOrderHtml("https://gpm.pl/shop/admin/order-details/id/55914", onResponseReceived);
+    $(document).ready(function(){
+        var inPost = new InPostManager(PrivateData.organizationId, PrivateData.token);
+        inPost.tryConnect(function(response){response == false ? console.log("401: Unauthorized!") : 
+            inPost.getShipments(function(response){console.log(JSON.parse(response));});    
+        });
+        
+        inPost.addShipment({
+            lockerId: "WAW099",
+            email: "justanemail@gmail.com",
+            phone: "123456789",
+            first_name: "Adam",
+            last_name: "Sandler",
+            city: "Łódź",
+            building_number: "22",
+            street: "Główna",
+            post_code: "11-123"
+        }, function(response){console.log(response);});
+
+    });
 })();
