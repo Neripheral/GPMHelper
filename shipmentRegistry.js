9d0655f639d658b75f0a6a72ee28404d2258a3ec
Neripheral <neripheral@gmail.com>
2023-02-16 19:37:36 +0100

    feat: implement getAsPDF

--- shipmentRegistry.js
+++ shipmentRegistry.js
@@ -1,7 +1,7 @@
 class ShipmentRegistry{
     static registerShipment(order_id, title, inpost_id){
         const date = new Date();
-        var dateString = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        var dateString = `${date.getFullYear()}-${('0' + (date.getMonth()+1)).slice(-2)}-${('0' + date.getDate()).slice(-2)} ${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}:${('0' + date.getSeconds()).slice(-2)}`;
         var toSet = {};
         toSet[dateString] = {};
         toSet[dateString]["order_id"] = order_id;
@@ -18,9 +18,48 @@ class ShipmentRegistry{
         });
     }
 
-    static printRegistryToConsole(){
+    static performOnRegistry(toPerform){
         chrome.storage.local.get("inpost_registry", data => {
+            toPerform(data["inpost_registry"]);
+        });
+    }
+
+    static printRegistryToConsole(){
+        this.performOnRegistry((data) => {
             console.log(JSON.stringify(data));
+        });
+    }
+
+    static getAsPDF(){
+        this.performOnRegistry((data) => {
+            var pdf = new jsPDF({
+                orientation: 'p',
+                unit: 'mm',
+                format: 'a4',
+                putOnlyUsedFonts: true
+            });
+
+            var date = new Date();
+            var today = `${date.getFullYear()}-${('0' + (date.getMonth()+1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`;
+            pdf.text(today, 10, 15);
+
+            var textArr = [];
+            Object.values(data).forEach(value => {
+                const [date, shipment] = Object.entries(value)[0];
+                var match = date.match(/(?<date>[^\s]*)\s(?<time>[^\s]*)/);
+                console.log(today);
+                console.log(JSON.stringify(match));
+
+                if(today !== match.groups.date)
+                    return;
+
+                console.log(date);
+                console.log(shipment);
+                var shipmentText = `${match.groups.time} InPost: ${shipment.inpost_id} - nr ${shipment.order_id} od ${shipment.title}`;
+                textArr = textArr.concat(pdf.splitTextToSize(shipmentText, 180));
+            });
+            pdf.text(textArr, 2, 25);
+            pdf.save('testingPdf.pdf');
         });
     }
 }
