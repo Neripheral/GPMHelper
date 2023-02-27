805b60c607ce9a26278bfa77a7271c3aa097c6fd
Neripheral <neripheral@gmail.com>
2023-02-27 23:28:27 +0100

    feat: implement pagination to pdf creator

--- shipmentRegistry.js
+++ shipmentRegistry.js
@@ -38,27 +38,30 @@ class ShipmentRegistry{
                 format: 'a4',
                 putOnlyUsedFonts: true
             });
-
+			
             var date = new Date();
             var today = `${date.getFullYear()}-${('0' + (date.getMonth()+1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`;
             pdf.text(today, 10, 15);
-
+			var count = 1;
             var textArr = [];
             Object.values(data).forEach(value => {
                 const [date, shipment] = Object.entries(value)[0];
                 var match = date.match(/(?<date>[^\s]*)\s(?<time>[^\s]*)/);
-                console.log(today);
-                console.log(JSON.stringify(match));
 
                 if(today !== match.groups.date)
                     return;
 
-                console.log(date);
-                console.log(shipment);
-                var shipmentText = `${match.groups.time} InPost: ${shipment.inpost_id} - nr ${shipment.order_id} od ${shipment.title}`;
-                textArr = textArr.concat(pdf.splitTextToSize(shipmentText, 180));
+                var shipmentText = `${('0' + count).slice(-2)}. ${shipment.inpost_id} - ${shipment.order_id} ${shipment.title}`.replace(/[\u0100-\uFFFF]/g,'?');
+                textArr = textArr.concat(pdf.splitTextToSize(shipmentText, 200));
+				count++;
+                if(textArr.length > 40){
+                    textArr.concat(["..."]);
+                    pdf.text(textArr, 5, 25);
+                    pdf.addPage();
+                    textArr = [];
+                }
             });
-            pdf.text(textArr, 2, 25);
+            pdf.text(textArr, 5, 25);
             pdf.save('testingPdf.pdf');
         });
     }
